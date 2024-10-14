7de413fb1588a88efdf1f124fa890d85b0477f23
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-10-14 20:45:34 +0200

    Array Oppgave

---
+++ script.js
@@ -0,0 +1,40 @@
+//Model
+let app = document.getElementById('App')
+let listOfRandomCrap = ['Brexit', 'Cheddar Cheese', 'Locomotive','Bird In Motion','Uganda']
+let crapGoesHere = ''
+
+
+
+
+
+//View
+updateView();
+function updateView(){
+    app.innerHTML= /*HTML*/`<div>${crapGoesHere}</div> <button onclick="printList()">Print Random Crap</button>`
+}
+
+
+
+
+
+
+
+
+
+//Controller
+function printList(){
+    for (let i = 0; i < listOfRandomCrap.lenghtr; i++){
+        crapGoesHere += listOfRandomCrap[i];
+        console.log(crapGoesHere) 
+    }
+    updateView();  
+   
+}
+
+
+
+
+
+
+
+
